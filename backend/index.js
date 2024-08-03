9b9dd59e9d135c959cd31cb2f2703570248f527e
Muhammad Ahsan <muhammadahsannoushad@gmail.com>
2024-08-03 16:57:06 +0500

    create index.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Ahsan!')
+// })
+
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
